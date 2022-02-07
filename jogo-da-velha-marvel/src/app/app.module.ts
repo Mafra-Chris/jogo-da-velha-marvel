8f1528dc4c13802466661bd00e427b2cd8999a6e
Chris <mafradesouza.cs@gmail.com>
2022-02-07 16:02:14 -0300

    app: import modules and create footer

--- jogo-da-velha-marvel/src/app/app.module.ts
+++ jogo-da-velha-marvel/src/app/app.module.ts
@@ -5,18 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
+import { ScoreboardComponent } from './scoreboard/scoreboard.component';
+import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
+import { PlayerComponent } from './player/player.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { StoreModule } from '@ngrx/store';
+import { playerReducer } from './player.reducer';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SquareComponent,
-    BoardComponent
+    BoardComponent,
+    ScoreboardComponent,
+    TicTacToeComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    StoreModule.forRoot({ player: playerReducer }, {}),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    library.add(fas)
+  }
+}
