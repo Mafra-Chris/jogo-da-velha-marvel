9cfe7d47855ec0e563648c62ec3ac54f599ab684
Chris <mafradesouza.cs@gmail.com>
2022-02-07 15:58:26 -0300

    create scoreboard container

---
+++ jogo-da-velha-marvel/src/app/scoreboard/scoreboard.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-scoreboard',
+  templateUrl: './scoreboard.component.html',
+  styleUrls: ['./scoreboard.component.scss']
+})
+export class ScoreboardComponent {
+
+  @Input() xScore: number
+  @Input() oScore: number
+
+}
